16db1b6771c0e7d7cbf28561ba7541eaa3890992
Drunken Cat <songjh123123@outlook.com>
2024-05-10 13:00:17 +0800

    将预载资源使用的正则改为静态资源，优化了运行效率

--- Utilities/StringExtension.cs
+++ Utilities/StringExtension.cs
@@ -10,13 +10,21 @@ public static class StringExtensions
     /// <summary>
     /// 解析命令中的各个元素并返回一个存储字符串键值对的字典。
     /// </summary>
+    private static readonly Regex CommandRegex = new Regex(
+      @"\s*(.*?)\s*=\s*(?:['""](.*?)['""]|([\w.-]+))\s*(?:,|$)",
+      RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
     public static StringDict ToCommandSet(this string input, string sep1 = ",", string sep2 = "=",
         bool isToLower = true)
     {
         // Prepare the regex pattern based on sep1 and sep2
-        var commandPattern =
-            $@"\s*(.*?)\s*{Regex.Escape(sep2)}\s*(?:['""](.*?)['""]|([\w.-]+))\s*{Regex.Escape(sep1)}?";
-        var regex = new Regex(commandPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        var regex = CommandRegex;
+        if (sep1 != "," || sep2 != "=")
+        {
+            var commandPattern =
+              $@"\s*(.*?)\s*{Regex.Escape(sep2)}\s*(?:['""](.*?)['""]|([\w.-]+))\s*{Regex.Escape(sep1)}?";
+            regex = new Regex(commandPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        }
         var matches = regex.Matches(input);
 
         var result = new StringDict();
@@ -33,7 +41,7 @@ public static class StringExtensions
 
         // Handling the case where no matches are found but a single pair might exist
         if (result.Count != 0 || matches.Count != 0) return result;
-        var singleMatch = Regex.Match(input, commandPattern);
+        var singleMatch = regex.Match(input);
         if (singleMatch.Success)
         {
             var key = singleMatch.Groups[1].Value;
